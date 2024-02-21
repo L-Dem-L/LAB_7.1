9583faafddabf208519d65dec410e7af343fe18b
Demian Rymar <ua.demyan@gmail.com>
2024-02-21 17:36:40 +0200

    Fixed

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -64,6 +64,51 @@ namespace UnitTest
 			delete[] z;
 		}
 
+		TEST_METHOD(TestPrint)
+		{
+			const int rowCount = 8;
+			const int colCount = 6;
+			int** z = new int* [rowCount];
+			for (int i = 0; i < rowCount; ++i)
+				z[i] = new int[colCount];
+
+			int testArray[rowCount][colCount] = {
+				{1, 2, 3, 4, 5, 6},
+				{7, 8, 9, 10, 11, 12},
+				{13, 14, 15, 16, 17, 18},
+				{19, 20, 21, 22, 23, 24},
+				{25, 26, 27, 28, 29, 30},
+				{31, 32, 33, 34, 35, 36},
+				{37, 38, 39, 40, 41, 42},
+				{43, 44, 45, 46, 47, 48}
+			};
+
+			for (int i = 0; i < rowCount; ++i)
+				for (int j = 0; j < colCount; ++j)
+					z[i][j] = testArray[i][j];
+
+			std::stringstream buffer;
+			std::streambuf* old = std::cout.rdbuf(buffer.rdbuf()); // Redirect cout
+			Print(z, rowCount, colCount);
+			std::cout.rdbuf(old); // Restore cout
+
+			std::string printedOutput = buffer.str();
+			std::string expectedOutput = "\n   1   2   3   4   5   6\n"
+				"   7   8   9  10  11  12\n"
+				"  13  14  15  16  17  18\n"
+				"  19  20  21  22  23  24\n"
+				"  25  26  27  28  29  30\n"
+				"  31  32  33  34  35  36\n"
+				"  37  38  39  40  41  42\n"
+				"  43  44  45  46  47  48\n\n";
+			Assert::AreEqual(expectedOutput, printedOutput);
+
+			for (int i = 0; i < rowCount; ++i)
+				delete[] z[i];
+			delete[] z;
+		}
+
+
 		TEST_METHOD(TestCalc)
 		{
 			const int rowCount = 8;
